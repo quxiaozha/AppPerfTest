442372ecb47caed1fc2c57cde355d5bb02065e0e
quxiaozha <quxun1992@126.com>
2023-04-04 23:58:35 +0800

    update typo

--- AndroidAppPerfTool/src/main/java/com/quxiaozha/util/AdbUtil.java
+++ AndroidAppPerfTool/src/main/java/com/quxiaozha/util/AdbUtil.java
@@ -3,7 +3,6 @@ package com.quxiaozha.util;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.math.BigDecimal;
